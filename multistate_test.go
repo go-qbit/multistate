d8bf9a4a12dbfb133d6aa0bdc6fde72b6ac3fe1a
Denis Karataev <dskarataev@gmail.com>
2024-06-24 21:51:42 +0700

    Get multistates by state ids that satisfy the rule: (a and b) and not (c or d)

--- multistate_test.go
+++ multistate_test.go
@@ -213,3 +213,163 @@ func TestMultistate_DoAction2(t *testing.T) {
 
 	// ioutil.WriteFile("/tmp/graph_wo.svg", []byte(mst.GetGraphSVG()), 0644)
 }
+
+func TestMultistate_GetMultistatesByRequiredAndForbiddenStateIds_NoError(t *testing.T) {
+	mst := multistate.New("New")
+
+	signedA := mst.MustAddState(0, "signed_a", "Signed A")
+	signedB := mst.MustAddState(1, "signed_b", "Signed B")
+	signedC := mst.MustAddState(2, "signed_c", "Signed C")
+	signedD := mst.MustAddState(3, "signed_d", "Signed D")
+	signedE := mst.MustAddState(4, "signed_e", "Signed E")
+
+	mst.MustAddAction(
+		"sign_a", "Sign A", Empty(),
+		multistate.States{signedA}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustAddAction(
+		"sign_b", "Sign B", Empty(),
+		multistate.States{signedB}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustAddAction(
+		"sign_c", "Sign C", And(Or(signedA, signedB), Not(signedC)),
+		multistate.States{signedC}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustAddAction(
+		"sign_d", "Sign D", And(Or(signedC, signedE), Not(signedD)),
+		multistate.States{signedD}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustAddAction(
+		"sign_e", "Sign E", And(Or(signedC, signedD), Not(signedE)),
+		multistate.States{signedE}, multistate.States{signedC},
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustCompile()
+
+	requiredIds := []string{"signed_a", "signed_c"}
+	forbiddenIds := []string{"signed_d"}
+
+	// Two options do satisfy the requirement:
+	// 1. signed_a (1) + signed_c (4) = 5
+	// 2. signed_a (1) + signed_c (4) + signed_e (16) = 21
+	expectedResult := []uint64{5, 21}
+
+	msts, err := mst.GetMultistatesByRequiredAndForbiddenStateIds(requiredIds, forbiddenIds)
+
+	assert.Equal(t, expectedResult, msts)
+	assert.NoError(t, err)
+}
+
+func TestMultistate_GetMultistatesByRequiredAndForbiddenStateIds_ErrorInvalidRequiredStateId(t *testing.T) {
+	mst := multistate.New("New")
+
+	signedA := mst.MustAddState(0, "signed_a", "Signed A")
+	signedB := mst.MustAddState(1, "signed_b", "Signed B")
+	signedC := mst.MustAddState(2, "signed_c", "Signed C")
+
+	mst.MustAddAction(
+		"sign_a", "Sign A", Empty(),
+		multistate.States{signedA}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustAddAction(
+		"sign_b", "Sign B", Empty(),
+		multistate.States{signedB}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustAddAction(
+		"sign_c", "Sign C", And(Or(signedA, signedB), Not(signedC)),
+		multistate.States{signedC}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustCompile()
+
+	requiredIds := []string{"signed_a", "signed_e"}
+	forbiddenIds := []string{"signed_d"}
+
+	msts, err := mst.GetMultistatesByRequiredAndForbiddenStateIds(requiredIds, forbiddenIds)
+
+	assert.Equal(t, []uint64(nil), msts)
+	assert.ErrorIs(t, err, multistate.ErrInvalidState)
+}
+
+func TestMultistate_GetMultistatesByRequiredAndForbiddenStateIds_ErrorInvalidForbiddenStateId(t *testing.T) {
+	mst := multistate.New("New")
+
+	signedA := mst.MustAddState(0, "signed_a", "Signed A")
+	signedB := mst.MustAddState(1, "signed_b", "Signed B")
+	signedC := mst.MustAddState(2, "signed_c", "Signed C")
+
+	mst.MustAddAction(
+		"sign_a", "Sign A", Empty(),
+		multistate.States{signedA}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustAddAction(
+		"sign_b", "Sign B", Empty(),
+		multistate.States{signedB}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustAddAction(
+		"sign_c", "Sign C", And(Or(signedA, signedB), Not(signedC)),
+		multistate.States{signedC}, nil,
+		func(_ context.Context, _ interface{}, _ ...interface{}) error {
+			return nil
+		},
+		nil,
+	)
+
+	mst.MustCompile()
+
+	requiredIds := []string{"signed_a", "signed_c"}
+	forbiddenIds := []string{"signed_e"}
+
+	msts, err := mst.GetMultistatesByRequiredAndForbiddenStateIds(requiredIds, forbiddenIds)
+
+	assert.Equal(t, []uint64(nil), msts)
+	assert.ErrorIs(t, err, multistate.ErrInvalidState)
+}
